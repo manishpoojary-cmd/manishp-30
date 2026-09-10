4b2198d8918f7f5088c1c0e94e9b4a3a8d10f3fc
manishpoojary-cmd <manishpoojary6030@gmail.com>
2026-09-10 11:37:05 +0530

    Add files via upload

---
+++ call.ref.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void add(int &x, int &y, int &sum)
+{
+    sum = x + y;
+}
+
+int main()
+{
+    int a,b,result;
+
+    cout<< "Enter two numbers: ";
+    cin >> a >> b;
+
+    add(a, b, result);
+
+     cout<< "sum=" << result << endl;
+
+     return 0;
+}
